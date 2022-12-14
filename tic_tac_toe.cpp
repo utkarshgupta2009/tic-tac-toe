8cd0801749e2558ee83298514a10331c5b0ebfe9
utkarsh gupta <117583741+utkarshgupta2009@users.noreply.github.com>
2022-12-14 15:21:31 +0530

    Update tic_tac_toe.cpp

--- tic_tac_toe.cpp
+++ tic_tac_toe.cpp
@@ -1,37 +1,30 @@
 #include<iostream>
 #include<stdlib.h>
 using namespace std;
-char a='1';
-char b='2';
-char c='3';
-char d='4';
-char e='5';
-char f='6';
-char g='7';
-char h='8';
-char i='9';
-int assign;
-int checking[9];
-int z;
-int count=1;
-int temp=1;
+
+char a='1' , b='2' , c='3' , d='4' , e='5' , f='6' , g='7' , h='8' , i='9';
+
+int assign, checking[9], z, count=1, temp=1;
 
 
 void input(int o){
     if((a=='0' && b=='0' && c=='0')||(d=='0' && e=='0' && f=='0')||(g=='0' && h=='0' && i=='0')||(a=='0' && d=='0' && g=='0')||(b=='0' && e=='0' && h=='0')||(c=='0' && f=='0' && i=='0')||(a=='0' && e=='0' && i=='0')||(c=='0' && e=='0' && g=='0')){
                 cout<<" "<<endl;
                 cout<<"PLAYER 1 WINS"<<endl;  
+                cout<<" "<<endl;
                 exit(0);
 }
 
     if((a=='X' && b=='X' && c=='X'||d=='X' && e=='X' && f=='X'||g=='X' && h=='X' && i=='X'||a=='X' && d=='X' && g=='X'||b=='X' && e=='X' && h=='X'||c=='X' && f=='X' && i=='X'||a=='X' && e=='X' && i=='X'||c=='X' && e=='X' && g=='X')){
                 cout<<" "<<endl;
                 cout<<"PLAYER 2 WINS"<<endl;
+                cout<<" "<<endl;
                 exit(0);
             }
     if(count==10){
         cout<<" "<<endl;
         cout<<"IT IS A TIE, NO ONE WINS"<<endl;
+        cout<<" "<<endl;
         exit(0);
     }
     cout<<" "<<endl;
@@ -79,7 +72,7 @@ cout<<"                                             "<<endl;
 cout<<" "<<"* ENTER THE NUMBER WHERE YOU WANT TO DISPLAY X OR 0"<<endl;
 cout<<"                                             "<<endl;
 cout<<"                                             "<<endl;
-cout<<"                PLAYER X= 0               PLAYER 2= X"<<endl;
+cout<<"                PLAYER 1= 0               PLAYER 2= X"<<endl;
 cout<<"                                             "<<endl;
 
 structure();
